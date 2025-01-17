04bb1bd321d475591561b29ccd48c0f4e1e14d71
Jakub.Grezl <jakub.grezl@outlook.cz>
2025-01-17 15:53:26 +0100

    visual changes in settings tab

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,195 +1,193 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
-    StyleSheet,
-    SafeAreaView,
-    ScrollView,
-    View,
-    Text,
-    TouchableOpacity,
-    Image,
-} from 'react-native';
-import {Picker} from '@react-native-picker/picker';
-import {getColors} from "@/constants/Colors";
-import { User } from "@/types/user"
-import {Collapsible} from "@/components/Collapsible";
-import {router} from "expo-router"
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import { getColors } from "@/constants/Colors";
+import { User } from "@/types/user";
+import { Collapsible } from "@/components/Collapsible";
+import { router } from "expo-router";
 
-
-import {storeData, getData} from "@/hooks/setStorageData"
+import { storeData, getData } from "@/hooks/setStorageData";
 
 let colors = getColors();
 
 export default function Settings() {
-    const [user, setUser] = useState<User>({
-        name: "Chcispat",
-        home: {
-            name: "Můj domov",
-            latitude: "55",
-            longitude: "55",
-        },
-    });
+  const [user, setUser] = useState<User>({
+    name: "Chcispat",
+    home: {
+      name: "Můj domov",
+      latitude: "55",
+      longitude: "55",
+    },
+  });
 
-    const [languageForm, setLanguageForm] = useState<string>("auto");
-    const [themeForm, setThemeForm] = useState<string>("auto");
+  const [languageForm, setLanguageForm] = useState<string>("auto");
+  const [themeForm, setThemeForm] = useState<string>("auto");
 
-    return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <View style={styles.profile}>
-                <TouchableOpacity
-                    onPress={() => {
-                        router.push("/auth")
-                    }}>
-                    <View style={styles.profileAvatarWrapper}>
-                        {user?.image ? (<Image
-                            alt="Profile picture"
-                            source={{uri: user.image}}
-                            style={styles.profileAvatar} />) : (  <Image
-                            alt="Profile picture"
-                            source={require("@/assets/images/blank.png")}
-                            style={styles.profileAvatar} />)}
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.profile}>
+        <TouchableOpacity
+          onPress={() => {
+            router.push("/auth");
+          }}
+        >
+          <View style={styles.profileAvatarWrapper}>
+            {user?.image ? (
+              <Image
+                alt="Profile picture"
+                source={{ uri: user.image }}
+                style={styles.profileAvatar}
+              />
+            ) : (
+              <Image
+                alt="Profile picture"
+                source={require("@/assets/images/blank.png")}
+                style={styles.profileAvatar}
+              />
+            )}
 
-                        <Text style={styles.profileName} >
-                            {user?.name}
-                        </Text>
-                        <Text style={styles.profileAddress} >
-                            {user?.home.name}
-                        </Text>
-                    </View>
-                </TouchableOpacity>
-            </View>
-            <ScrollView>
-                <View style={styles.section}>
-                    <Text style={styles.sectionTitle}>Preferences</Text>
-
-                    <Collapsible iconName="moon" text="Theme">
-
-                    <Picker
-                        selectedValue={themeForm}
-                        onValueChange={(itemValue, itemIndex) => {
-                            storeData({storeKey: "theme", value: themeForm}).then(() => {
-                                    getData({storeKey: "theme"}).then((r) => {
-                                        if (typeof r == "string") setThemeForm(r)
-                                    });
-                                }
-                            )
-                            setThemeForm(itemValue);
-                        }
-                        }
-                        itemStyle={{color: colors.text}}
-                    >
-
-                        <Picker.Item label="Dark" value="dark"  />
-                        <Picker.Item label="Light" value="light" />
-                        <Picker.Item label="System" value="auto" />
-                    </Picker>
-                    </Collapsible>
-                    <Collapsible iconName="globe" text="Language">
-                        <Picker
-                            selectedValue={languageForm}
-                            onValueChange={(itemValue, itemIndex) => {
-                                storeData({storeKey: "language", value: languageForm}).then(() => {
-                                    getData({storeKey: "language"}).then((r) => {
-                                        if (typeof r == "string") setLanguageForm(r)
-                                    });
-                                });
-                                setLanguageForm(itemValue);
-                            }
-                            }
-                            itemStyle={{color: colors.text}}
-                        >
-
-                            <Picker.Item label="Czech" value="czech"  />
-                            <Picker.Item label="English" value="english" />
-                            <Picker.Item label="System" value="auto" />
-                        </Picker>
-                    </Collapsible>
-                </View>
-            </ScrollView>
-        </SafeAreaView>
-    );
+            <Text style={styles.profileName}>{user?.name}</Text>
+            <Text style={styles.profileAddress}>{user?.home.name}</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <ScrollView>
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Preferences</Text>
+          <Collapsible iconName="moon" text="Theme">
+            <Picker
+              selectedValue={themeForm}
+              onValueChange={(itemValue, itemIndex) => {
+                storeData({ storeKey: "theme", value: themeForm }).then(() => {
+                  getData({ storeKey: "theme" }).then((r) => {
+                    if (typeof r == "string") setThemeForm(r);
+                  });
+                });
+                setThemeForm(itemValue);
+              }}
+              itemStyle={{ color: colors.text }}
+            >
+              <Picker.Item label="Dark" value="dark" />
+              <Picker.Item label="Light" value="light" />
+              <Picker.Item label="System" value="auto" />
+            </Picker>
+          </Collapsible>
+          <Collapsible iconName="globe" text="Language">
+            <Picker
+              selectedValue={languageForm}
+              onValueChange={(itemValue, itemIndex) => {
+                storeData({ storeKey: "language", value: languageForm }).then(
+                  () => {
+                    getData({ storeKey: "language" }).then((r) => {
+                      if (typeof r == "string") setLanguageForm(r);
+                    });
+                  }
+                );
+                setLanguageForm(itemValue);
+              }}
+              itemStyle={{ color: colors.text }}
+            >
+              <Picker.Item label="Czech" value="czech" />
+              <Picker.Item label="English" value="english" />
+              <Picker.Item label="System" value="auto" />
+            </Picker>
+          </Collapsible>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    /** Profile */
-    profile: {
-        padding: 24,
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    profileAvatarWrapper: {
-        position: 'relative',
-        alignItems: 'center',
-    },
-    profileAvatar: {
-        width: 72,
-        height: 72,
-        borderRadius: 9999,
-    },
-    profileAction: {
-        position: 'absolute',
-        right: -4,
-        bottom: -10,
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: 28,
-        height: 28,
-        borderRadius: 9999,
-        backgroundColor: '#007bff',
-    },
-    profileName: {
-        marginTop: 20,
-        fontSize: 19,
-        fontWeight: '600',
-        color: colors.text,
-        textAlign: 'center',
-    },
-    profileAddress: {
-        marginTop: 5,
-        fontSize: 16,
-        color: colors.subtitle,
-        textAlign: 'center',
-    },
-    /** Section */
-    section: {
-        paddingHorizontal: 24,
-    },
-    sectionTitle: {
-        paddingVertical: 12,
-        fontSize: 12,
-        fontWeight: '600',
-        color: '#9e9e9e',
-        textTransform: 'uppercase',
-        letterSpacing: 1.1,
-    },
-    /** Row */
-    row: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        height: 50,
-        backgroundColor: colors.background,
-        borderRadius: 8,
-        marginBottom: 12,
-        paddingHorizontal: 12,
-    },
-    rowIcon: {
-        width: 32,
-        height: 32,
-        borderRadius: 9999,
-        marginRight: 12,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    rowLabel: {
-        fontSize: 17,
-        fontWeight: '400',
-        color: colors.text,
-    },
-    rowSpacer: {
-        flexGrow: 1,
-        flexShrink: 1,
-        flexBasis: 0,
-    },
+  /** Profile */
+  profile: {
+    padding: 24,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  profileAvatarWrapper: {
+    position: "relative",
+    alignItems: "center",
+  },
+  profileAvatar: {
+    width: 72,
+    height: 72,
+    borderRadius: 9999,
+  },
+  profileAction: {
+    position: "absolute",
+    right: -4,
+    bottom: -10,
+    alignItems: "center",
+    justifyContent: "center",
+    width: 28,
+    height: 28,
+    borderRadius: 9999,
+    backgroundColor: "#007bff",
+  },
+  profileName: {
+    marginTop: 20,
+    fontSize: 19,
+    fontWeight: "600",
+    color: colors.text,
+    textAlign: "center",
+  },
+  profileAddress: {
+    marginTop: 5,
+    fontSize: 16,
+    color: colors.subtitle,
+    textAlign: "center",
+  },
+  /** Section */
+  section: {
+    paddingHorizontal: 24,
+    gap: 10,
+  },
+  sectionTitle: {
+    paddingTop: 12,
+    fontSize: 12,
+    fontWeight: "600",
+    color: "#9e9e9e",
+    textTransform: "uppercase",
+    letterSpacing: 1.1,
+  },
+  /** Row */
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    height: 50,
+    backgroundColor: colors.background,
+    borderRadius: 8,
+    marginBottom: 12,
+    paddingHorizontal: 12,
+  },
+  rowIcon: {
+    width: 32,
+    height: 32,
+    borderRadius: 9999,
+    marginRight: 12,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  rowLabel: {
+    fontSize: 17,
+    fontWeight: "400",
+    color: colors.text,
+  },
+  rowSpacer: {
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+  },
 });
--- components/Collapsible.tsx
+++ components/Collapsible.tsx
@@ -1,13 +1,28 @@
-import React, { PropsWithChildren, useState, useEffect, useRef } from 'react';
-import {Animated, StyleSheet, Text, TouchableOpacity, useColorScheme, View} from 'react-native';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import { getColors } from '@/constants/Colors';
-import {getColorReverseIcon} from "@/utils/theme";
-import {SFSymbols5_0} from "sf-symbols-typescript";
+import React, { PropsWithChildren, useState, useEffect, useRef } from "react";
+import {
+  Animated,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  useColorScheme,
+  View,
+} from "react-native";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import { getColors } from "@/constants/Colors";
+import { getColorReverseIcon } from "@/utils/theme";
+import { SFSymbols5_0 } from "sf-symbols-typescript";
 
 let colors = getColors();
 
-export function Collapsible({children, iconName, text}:{children: React.ReactNode, iconName: SFSymbols5_0, text: string} ) {
+export function Collapsible({
+  children,
+  iconName,
+  text,
+}: {
+  children: React.ReactNode;
+  iconName: SFSymbols5_0;
+  text: string;
+}) {
   const [isOpen, setIsOpen] = useState(false);
 
   const rotateAnim = useRef(new Animated.Value(0)).current;
@@ -16,7 +31,7 @@ export function Collapsible({children, iconName, text}:{children: React.ReactNod
   const toggleDrawer = () => {
     Animated.parallel([
       Animated.timing(rotateAnim, {
-        toValue: isOpen ? 1 : 0, // 1 for 180 degrees, 0 for 0 degrees
+        toValue: isOpen ? 0 : 1, // 1 for 180 degrees, 0 for 0 degrees
         duration: 300, // Duration in ms
         useNativeDriver: true, // Smooth animations
       }),
@@ -25,46 +40,54 @@ export function Collapsible({children, iconName, text}:{children: React.ReactNod
         duration: 300, // Duration of animation in ms
         useNativeDriver: false, // Height animations require `useNativeDriver: false`
       }),
-    ]).start();
-    setIsOpen(!isOpen);
+    ]).start(() => {
+      // Callback after animation completion
+      if (isOpen) setIsOpen(false);
+    });
+
+    if (!isOpen) {
+      setIsOpen(true);
+    }
   };
 
   const rotation = rotateAnim.interpolate({
     inputRange: [0, 1], // 0 is 0 degrees, 1 is 180 degrees
-    outputRange: ['0deg', '180deg'],
+    outputRange: ["0deg", "180deg"],
   });
 
   return (
     <View>
-      <TouchableOpacity
-        style={styles.heading}
-        onPress={toggleDrawer}
-        >
-
+      <TouchableOpacity style={styles.heading} onPress={toggleDrawer}>
         <IconSymbol
-            name={iconName}
-            size={18}
-            weight="medium"
-            color={colors.icon}
+          name={iconName}
+          size={18}
+          weight="medium"
+          color={colors.icon}
         />
 
         <Text style={styles.rowLabel}>{text}</Text>
 
         <View style={styles.rowSpacer}></View>
 
-        <Animated.View style={{
-          transform: [{ rotate: rotation }], // Apply rotation animation
-        }}>
-
+        <Animated.View
+          style={{
+            transform: [{ rotate: rotation }], // Apply rotation animation
+          }}
+        >
           <IconSymbol
-              name="chevron.up"
-              size={18}
-              weight="medium"
-              color={getColorReverseIcon()}
+            name="chevron.up"
+            size={18}
+            weight="medium"
+            color={getColorReverseIcon()}
           />
         </Animated.View>
       </TouchableOpacity>
-      <Animated.View style={[styles.drawer, { height: heightAnim }]}>
+      <Animated.View
+        style={[
+          styles.drawer,
+          { height: heightAnim, display: isOpen ? "flex" : "none" },
+        ]}
+      >
         {children}
       </Animated.View>
     </View>
@@ -74,14 +97,13 @@ export function Collapsible({children, iconName, text}:{children: React.ReactNod
 const styles = StyleSheet.create({
   heading: {
     display: "flex",
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
     height: 50,
     gap: 10,
     backgroundColor: colors.background,
     borderRadius: 8,
-    marginBottom: 12,
     paddingHorizontal: 12,
   },
   drawer: {
@@ -89,14 +111,13 @@ const styles = StyleSheet.create({
     overflow: "hidden", // Ensure content is clipped when collapsed
     borderRadius: 8,
     backgroundColor: colors.background,
-    display: "flex",
     flexDirection: "column",
     gap: 5,
-    marginBottom: 10,
+    marginTop: 10,
   },
   rowLabel: {
     fontSize: 17,
-    fontWeight: '400',
+    fontWeight: "400",
     color: colors.text,
   },
   rowSpacer: {
